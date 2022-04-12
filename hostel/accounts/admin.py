84555f9ddff9933fd5932f916497c6f927ab820a
Sruthinairpr <19cs086srut@ug.cusat.ac.in>
2022-04-12 18:59:40 +0530

    first commit

---
+++ hostel/accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Inmates
+
+admin.site.register(Inmates)
---
+++ hostel/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Inmates(models.Model):
+	Hostel_ID = models.CharField(max_length=30, null=True)
+	Name = models.CharField(max_length=100, null=True)
+	Room_Num = models.CharField(max_length=10, null=True)
+	Preference = models.CharField(max_length=20, null=True)
+	Present_Amount = models.IntegerField(null=True)
+	MC = models.IntegerField(null=True)
